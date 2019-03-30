ab61058aaa9689a924cc861577f02e7d294ba0b0
Bianca Botezatu <botezatubianca20@gmail.com>
2019-03-30 22:32:49 +0200

    server

---
+++ backend/server.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Employees(Resource):
+    def get(self):
+        return {'movies': [{'id':1, 'name':'50 Shades of Grey'},{'id':2, 'name':'50 Shades Darker'}]} 
+
+api.add_resource(Employees, '/movies') # Route_1
+
+if __name__ == '__main__':
+     app.run(port=5002)
