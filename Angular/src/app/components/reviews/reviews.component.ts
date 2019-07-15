aac4b76a24d6f1a740801f9deafdb118e75d3c59
Bianca Botezatu <botezatubianca20@gmail.com>
2019-07-16 00:44:43 +0300

    fixed bug reviews

--- Angular/src/app/components/reviews/reviews.component.ts
+++ Angular/src/app/components/reviews/reviews.component.ts
@@ -15,11 +15,27 @@ export class ReviewsComponent implements OnInit {
   ngOnInit() {
     this.reviewsService.getPositiveReviews().subscribe(res => {
       this.positive=res;
+      for(var i=0; i<this.positive.length; i++){
+        if((this.positive[i].content.includes("not") || this.positive[i].content.includes("Not"))  && this.negative[i].content.length<=25){
+          const index: number = this.positive.indexOf(this.positive[i]);
+          if (index !== -1) {
+              this.positive.splice(index, 1);
+           }   
+        }
+      }
       console.log(this.positive)
     })
 
     this.reviewsService.getNegativeReviews().subscribe(res => {
       this.negative=res;
+      for(var i=0; i<this.negative.length; i++){
+        if((this.negative[i].content.includes("not") || this.negative[i].content.includes("Not")) && this.negative[i].content.length<=25){
+          const index: number = this.negative.indexOf(this.negative[i]);
+          if (index !== -1) {
+              this.negative.splice(index, 1);
+           }   
+        }
+      }
       console.log(this.negative)
     })
 
